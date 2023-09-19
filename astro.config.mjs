5a281b4b07b05b97bcec6da1bf4e2cda891f08d4
Mark Leung <mark.leung@studiorotate.com>
2023-09-19 15:11:34 +0100

    Use static mode to test form submission

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,8 @@
+import { netlifyStatic } from '@astrojs/netlify'
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: netlifyStatic(),
+  compressHTML: true,
+});
